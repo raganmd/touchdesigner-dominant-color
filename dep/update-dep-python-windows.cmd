85bd5eeab9e39fd69bac1550c1b29fd7f0f5637a
Matthew Ragan <matthew.ragan@obscuradigital.com>
2018-11-25 21:45:12 -0800

    updates to gitignore added script to located depadancies in another folder update toe file for importing a new sys path

---
+++ dep/update-dep-python-windows.cmd
@@ -0,0 +1,10 @@
+:: Update dependencies
+
+:: make sure pip is up to date
+python -m pip install --upgrade pip
+
+:: pull phue
+pip install --target="%~dp0\python" numpy
+pip install --target="%~dp0\python" scipy
+pip install --target="%~dp0\python" cv2
+pip install --target="%~dp0\python" sklearn
