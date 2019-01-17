55711a58a03030269bdc57102a9cf4daa9a4469c
Matthew Ragan <raganmd@gmail.com>
2019-01-17 08:35:25 -0800

    Re-org to match standards

---
+++ dev/dep/DomColor/update-dep-python-windows.cmd
@@ -0,0 +1,7 @@
+:: Update dependencies
+
+:: make sure pip is up to date
+python -m pip install --upgrade pip
+
+:: install requirements
+pip install -r C:/projects/ragan/touchdesigner/_tools/touchdesigner-dominant-color/dev/dep/DomColor/requirements.txt --target="C:/projects/ragan/touchdesigner/_tools/touchdesigner-dominant-color/dev/dep/python"
--- dev/scripts/extHelperMOD.py
+++ dev/scripts/extHelperMOD.py
@@ -53,74 +53,103 @@ def Check_dep_path():
 		None
 	'''
  
-	Dep_path 		= '{}/dep'.format(project.folder)
-	module_path 	= '{}/dep/python/sklearn'.format(project.folder)
-	win_py_dep 		= '{}/dep/update-dep-python-windows.cmd'.format(project.folder)
-	mac_py_dep 		= '{}/dep/update-dep-python-mac.sh'.format(project.folder)
+	dep_path 			= '{}/dep'.format(project.folder)
+	python_path 		= '{}/dep/python'.format(project.folder)
+	scripts_reqs_path 	= '{proj}/dep/{name}'.format(proj=project.folder, name=parent().par.Name)
+	requirements 		= '{}/requirements.txt'.format(scripts_reqs_path)
+	reqs_dat 			= op('reqs')
+	phue_path 			= '{}/dep/python/phue.py'.format(project.folder)
+	win_py_dep 			= '{}/update-dep-python-windows.cmd'.format(scripts_reqs_path)
+	mac_py_dep 			= '{}/update-dep-python-mac.sh'.format(scripts_reqs_path)
 
  	# check to see if /dep is in the project folder
-	if os.path.isdir(Dep_path):
+	if os.path.isdir(dep_path):
 		pass
 	# create the direcotry if it's not there
 	else:
-		os.mkdir(Dep_path)
+		os.mkdir(dep_path)
 
-	# check to see if our requirements elements are in place
+ 	# check to see if /python is in the project folder
+	if os.path.isdir(python_path):
+		pass
+	# create the direcotry if it's not there
+	else:
+		os.mkdir(python_path)
+
+ 	# check to see if there's a scripts and requirements folder
+	if os.path.isdir(scripts_reqs_path):
+		pass
+	# create the direcotry if it's not there
+	else:
+		os.mkdir(scripts_reqs_path)
+
+	# check to see if the requirements txt is in place
+	if os.path.isfile(requirements):
+		pass
+	else:
+		reqs_file 	= open(requirements, 'w')
+		reqs_file.write(reqs_dat.text)
+		reqs_file.close()
+
+	# check to see if our auto-generaetd scripts are in place
 	has_win_py 		= os.path.isfile(win_py_dep)
 	has_mac_py 		= os.path.isfile(mac_py_dep)
 
-	win_py_txt 		= me.mod.extHelperMOD.win_dep()
-	mac_py_txt 		= me.mod.extHelperMOD.mac_dep()
-
-	if has_win_py and has_mac_py:
-		pass
-	else:
-		win_file 	= open(win_py_dep, 'w')
-		win_file.write(win_py_txt)
-		win_file.close()
-
-		mac_file 	= open(mac_py_dep, 'w')
-		mac_file.write(mac_py_txt)
-		mac_file.close()
+	win_py_txt 		= me.mod.extHelperMOD.win_dep(scripts_reqs_path, python_path)
+	mac_py_txt 		= me.mod.extHelperMOD.mac_dep(scripts_reqs_path, python_path)
 
 	# identify platform
 	osPlatform 		= platform.system()
 
-	# on windows run the file in command line
+	# on windows
 	if osPlatform == "Windows":
-		if os.path.isdir(module_path):
-			pass
-		else:
+		# create the script to handle grabbing our dependencies
+		req_file 	= open(win_py_dep, 'w')
+		req_file.write(win_py_txt)
+		req_file.close()
+
+		# check to see if there is anything in the python dep dir
+		# for now we'll assume that if there are files here we
+		# successfully installed our python dependencies
+		if len(os.listdir(python_path)) == 0:
 			subprocess.Popen([win_py_dep])
-	
-	elif osPlatform == "Darwin":
-		if os.path.isdir(module_path):
-			pass
 		else:
+			pass				
+	# on mac
+	elif osPlatform == "Darwin":
+		# create the script to handle grabbing our dependencies
+		mac_file 	= open(mac_py_dep, 'w')
+		mac_file.write(mac_py_txt)
+		mac_file.close()
+
+		# check to see if there is anything in the python dep dir
+		# for now we'll assume that if there are files here we
+		# successfully installed our python dependencies
+		if len(os.listdir(python_path)) == 0:
 			subprocess.Popen([mac_py_dep])
+		else:
+			pass
+
 	else:
 		pass
 
 	return
 
-def win_dep():
-    win_txt = '''
-:: Update dependencies
+def win_dep(requirementsPath, targetPath):
+	win_txt = ''':: Update dependencies
 
 :: make sure pip is up to date
 python -m pip install --upgrade pip
 
-:: pull phue
-pip install --target="%~dp0\python" numpy
-pip install --target="%~dp0\python" scipy
-pip install --target="%~dp0\python" cv2
-pip install --target="%~dp0\python" sklearn
-'''
+:: install requirements
+pip install -r {reqs}/requirements.txt --target="{target}"'''
 
-    return win_txt
+	formatted_win_txt = win_txt.format(reqs=requirementsPath, target=targetPath)
+	
+	return formatted_win_txt
 
-def mac_dep():
-    mac_txt = '''
+def mac_dep(requirementsPath, targetPath):
+	mac_txt = '''
 #!/bin/bash 
 
 dep=$(dirname "$0")
@@ -141,10 +170,7 @@ python3 get-pip.py
 # make sure pip is up to date
 python3 -m pip install --upgrade pip
 
-# pull phue
-python3 -m pip install --target=$dep$pythonDir numpy
-python3 -m pip install --target=$dep$pythonDir scipy
-python3 -m pip install --target=$dep$pythonDir cv2
-python3 -m pip install --target=$dep$pythonDir sklearn
-'''
-    return mac_txt
+# install requirements
+python3 -m pip install -r {reqs}/requirements.txt --target={target}'''
+	formatted_mac_txt = mac_txt.format(reqs=requirementsPath, target=targetPath)
+	return formatted_mac_txt
