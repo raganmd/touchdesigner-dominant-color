4ad1df9b79cafc2edd4c49124580f06d3f0aef64
Matthew Ragan <raganmd@gmail.com>
2019-06-16 23:27:46 -0700

    Conversion to Standard Structure
    Spring 099 Update #10b677
    Standard dir structure #10b5ap
    General reorg and clean-up

--- dev/dep/DomColor/update-dep-python-windows.cmd
+++ dev/dep/DomColor/update-dep-python-windows.cmd
@@ -1,7 +1,7 @@
 :: Update dependencies
 
 :: make sure pip is up to date
-python -m pip install --upgrade pip
+python -m pip install --user --upgrade pip
 
 :: install requirements
-pip install -r C:/projects/ragan/touchdesigner/_tools/touchdesigner-dominant-color/dev/dep/DomColor/requirements.txt --target="C:/projects/ragan/touchdesigner/_tools/touchdesigner-dominant-color/dev/dep/python"
+pip install -r E:/github/ragan-git/touchdesigner/_tools/touchdesigner-dominant-color/dev/dep/DomColor/requirements.txt --target="E:/github/ragan-git/touchdesigner/_tools/touchdesigner-dominant-color/dev/dep/python"
