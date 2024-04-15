96ac960cea9164667dbd09b5b9c9986673f04e4b
Matthew Ragan <raganmd@gmail.com>
2024-04-15 13:59:50 -0700

    Changes to modern workflow

--- dev/dep/DomColor/update-dep-python-windows.cmd
+++ dev/dep/DomColor/update-dep-python-windows.cmd
@@ -4,4 +4,4 @@
 python -m pip install --user --upgrade pip
 
 :: install requirements
-pip install -r E:/github/ragan-git/touchdesigner/_tools/touchdesigner-dominant-color/dev/dep/DomColor/requirements.txt --target="E:/github/ragan-git/touchdesigner/_tools/touchdesigner-dominant-color/dev/dep/python"
+pip install -r D:/_repos/github/ragan-git/touchdesigner/_tools/touchdesigner-dominant-color/dev/dep/DomColor/requirements.txt --target="D:/_repos/github/ragan-git/touchdesigner/_tools/touchdesigner-dominant-color/dev/dep/python"
