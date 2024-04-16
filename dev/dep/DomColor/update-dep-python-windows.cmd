217b8cd34af2deaf4cabab7e1b9c6476ccad7396
Matthew Ragan <raganmd@gmail.com>
2024-04-15 22:16:34 -0700

    Updates to approach
    Fixes up some small issues
    Moves pip install to Popen call
    Uses Python shipped with TD for install operations

--- dev/dep/DomColor/update-dep-python-windows.cmd
+++
@@ -1,7 +0,0 @@
-:: Update dependencies
-
-:: make sure pip is up to date
-python -m pip install --user --upgrade pip
-
-:: install requirements
-pip install -r D:/_repos/github/ragan-git/touchdesigner/_tools/touchdesigner-dominant-color/dev/dep/DomColor/requirements.txt --target="D:/_repos/github/ragan-git/touchdesigner/_tools/touchdesigner-dominant-color/dev/dep/python"
